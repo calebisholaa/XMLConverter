acba369f0171f35fd81bd7ad39a11861c978c608
calebisholaa <isholaa@etsu.edu>
2023-03-09 14:58:17 -0500

    Initial Commit

---
+++ CSV2XMLConvert/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace CSV2XMLConvert
+{
+    partial class from1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(from1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.displayText = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 589);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 24);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open CSV";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // displayText
+            // 
+            this.displayText.Location = new System.Drawing.Point(26, 82);
+            this.displayText.Multiline = true;
+            this.displayText.Name = "displayText";
+            this.displayText.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.displayText.Size = new System.Drawing.Size(750, 473);
+            this.displayText.TabIndex = 1;
+            this.displayText.WordWrap = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(679, 584);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 29);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Convert to XML";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // from1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 636);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.displayText);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "from1";
+            this.Text = "XMLConverter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox displayText;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ CSV2XMLConvert/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Windows.Forms.LinkLabel;
+
+namespace CSV2XMLConvert
+{
+    public partial class from1 : Form
+    {
+
+        string filePath = "";
+        string fileContent = "";
+        public from1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenAndReadFile();
+            
+        }
+
+
+        private void OpenAndReadFile()
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    this.filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                    }
+                }
+            }
+
+            this.displayText.Text = fileContent;
+
+            // Console.WriteLine(fileContent + "From Open");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //SaveFile(fileContent);
+            this.displayText.Text = SaveFile(fileContent);
+        }
+
+        private void Convert2XML(string fileContent)
+        {
+            var xml = new XElement("TopElement",
+                fileContent.Select(line => new XElement("Item",
+                fileContent.Split(',')
+             .Select((column, index) => new XElement("Column" + index, column)))));
+
+            // xml.Save(@"C:\xmlout.xml");
+        }
+
+
+        private string SaveFile(string file)
+        {
+            string result = "";
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            saveFileDialog.Filter = "xml files (*.xml)|*.xml";
+            saveFileDialog.FilterIndex = 2;
+            saveFileDialog.RestoreDirectory = true;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+
+
+                //var header = File.ReadLines(filePath).First().Split(','); //Read header
+                //var lines = File.ReadAllLines(filePath).Skip(1); //Skip header row        
+
+                ////format the xml how you want
+                //var xml = new XElement("EmpDetails",
+                //    lines.Select(line => new XElement("Item",
+                //        line.Split(',').Select((column, index) => new XElement(header[index], column)))));
+
+
+                // Read all lines from the CSV file
+                var fileContent = File.ReadAllLines(filePath);
+
+
+                // Remove any white space from each line
+                fileContent = fileContent.Select(line => Regex.Replace(line, @"\s", "")).ToArray();
+
+
+                // Split the first line into headers, and replace any spaces with underscores
+                string[] headers = fileContent[0].Split(',').Select(x => x.Replace(" ", "_").Replace("/", "_").Trim('\"')).ToArray();
+
+                // Skip the first line (headers)
+                fileContent = fileContent.Skip(1).ToArray();
+
+
+                // Create a new XML document with a top-level element
+                var xml = new XElement("TopElement");
+
+                // Iterate over the rows in the CSV file
+                foreach (string line in fileContent)
+                {
+                    
+                    // Split the line into columns, and trim any double quotes
+                    string[] columns = line.Split(',').Select(x => x.Trim('\"')).ToArray();
+                  
+
+                
+                    // If the row has no data, create a row element with "null" values for each column
+                    if (string.IsNullOrWhiteSpace(line))
+                    {
+                        xml.Add(new XElement("Item", headers.Select(header => new XElement(header, "null"))));
+                    }
+                    else
+                    {
+                        // Otherwise, create a row element with values from each column
+                        var item = new XElement("Item");
+                        for (int i = 0; i < headers.Length && i < columns.Length; i++)
+                        {
+                            var value = string.IsNullOrWhiteSpace(columns[i]) ? "null" : columns[i];
+                            item.Add(new XElement(headers[i], value));
+                        }
+                        xml.Add(item);
+                    }
+                }
+
+
+                #region
+                //// Read all lines from the CSV file
+                //var fileContent = File.ReadAllLines(filePath);
+
+                //// Split the first line into headers, and replace any spaces with underscores
+                //string[] headers = fileContent[0].Split(',').Select(x => x.Replace(" ", "_").Replace("/", "_").Trim('\"')).ToArray();
+
+                //// Skip the first line (headers)
+                //fileContent = fileContent.Skip(1).ToArray();
+
+                //// Create a new XML document with a top-level element
+                //var xml = new XElement("TopElement");
+
+                //// Create a variable to hold the current row
+                //string currentRow = "";
+
+                //// Iterate over the rows in the CSV file
+                //foreach (string line in fileContent)
+                //{
+                //    // If the current row is not empty, add it to the XML document
+                //    if (!string.IsNullOrWhiteSpace(currentRow))
+                //    {
+                //        // Split the row into columns, and trim any double quotes
+                //        string[] columns = currentRow.Split(',').Select(x => x.Trim('\"')).ToArray();
+
+                //        // Create a row element with values from each column
+                //        var item = new XElement("Item");
+                //        for (int i = 0; i < headers.Length && i < columns.Length; i++)
+                //        {
+                //            var value = string.IsNullOrWhiteSpace(columns[i]) ? "null" : columns[i];
+                //            item.Add(new XElement(headers[i], value));
+                //        }
+                //        xml.Add(item);
+                //    }
+
+                //    // Set the current row to the next non-empty row in the CSV file
+                //    currentRow = line.Trim();
+
+                //    // If the current row is empty, continue to the next row
+                //    if (string.IsNullOrWhiteSpace(currentRow))
+                //    {
+                //        continue;
+                //    }
+
+                //    // Check if the current row is a continuation of the previous row
+                //    if (char.IsLetterOrDigit(currentRow[0]))
+                //    {
+                //        currentRow = "," + currentRow;
+                //    }
+                //    else
+                //    {
+                //        // If the current row is not a continuation of the previous row, add it to the XML document
+                //        xml.Add(new XElement("Item", headers.Select(header => new XElement(header, "null"))));
+                //    }
+                //}
+
+                //// Add the last row to the XML document
+                //if (!string.IsNullOrWhiteSpace(currentRow))
+                //{
+                //    // Split the row into columns, and trim any double quotes
+                //    string[] columns = currentRow.Split(',').Select(x => x.Trim('\"')).ToArray();
+
+                //    // Create a row element with values from each column
+                //    var item = new XElement("Item");
+                //    for (int i = 0; i < headers.Length && i < columns.Length; i++)
+                //    {
+                //        var value = string.IsNullOrWhiteSpace(columns[i]) ? "null" : columns[i];
+                //        item.Add(new XElement(headers[i], value));
+                //    }
+                //    xml.Add(item);
+                //}
+                #endregion
+
+                xml.Save(saveFileDialog.FileName);
+                result = xml.ToString();
+
+
+                //var xml = new XElement("TopElement",
+                //fileContent.Select(line => new XElement("Item",
+                //    fileContent.Split(',')
+                //    .Select((column, index) => new XElement("Column" +index, column)))));
+
+
+
+            }
+
+            return result;
+        }
+    }
+}
